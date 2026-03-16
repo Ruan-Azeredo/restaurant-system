e080c4b76f44afac66dc429b54c4da59584b1acd
Ruan-M2 <ruanazeredo@gmail.com>
2026-03-16 13:48:51 -0300

    wip: improve redis connection

--- apps/api/src/@infra/queue/redisConnection.ts
+++ apps/api/src/@infra/queue/redisConnection.ts
@@ -1,5 +1,7 @@
 export const redisConnectionOptions = {
   host: process.env.REDIS_HOST || "localhost",
   port: parseInt(process.env.REDIS_PORT || "6379", 10),
+  username: process.env.REDIS_USERNAME || "default",
+  password: process.env.REDIS_PASSWORD || undefined,
   maxRetriesPerRequest: null as null, // Required by BullMQ
 };
